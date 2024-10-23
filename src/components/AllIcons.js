8c5d8347a6280c69f51073e62644cced1726880e
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-23 16:23:48 -0500

    Listo of icons weather

---
+++ src/components/AllIcons.js
@@ -0,0 +1,23 @@
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzie_icon from "../assets/drizzie.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import cloudy_icon from "../assets/cloudy.png";
+
+export const allicons = {
+  "01d": clear_icon,
+  "01n": clear_icon,
+  "02d": cloud_icon,
+  "02n": cloud_icon,
+  "03d": cloud_icon,
+  "03n": cloudy_icon,
+  "04d": cloudy_icon,
+  "04n": drizzie_icon,
+  "09d": rain_icon,
+  "09n": rain_icon,
+  "10d": rain_icon,
+  "10n": rain_icon,
+  "13d": snow_icon,
+  "13n": snow_icon,
+};
