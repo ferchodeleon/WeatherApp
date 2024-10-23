2c2669b2165bc6da768b01b0c504b5169614872b
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-23 16:28:00 -0500

    Add header and translate language

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import global_en from "./translate/en/global.json";
+import global_es from "./translate/es/global.json";
+import { Header } from "./components/Header.jsx";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_en,
+    },
+    es: {
+      global: global_es,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <I18nextProvider i18n={i18next}>
+      <Header />
+      <App />
+    </I18nextProvider>
+  </StrictMode>
+);
