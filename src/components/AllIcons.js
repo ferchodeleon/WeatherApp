77715603103ee993208a48cf3ed1fe5715051cd9
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-24 17:24:29 -0500

    Change icons weather

--- src/components/AllIcons.js
+++ src/components/AllIcons.js
@@ -1,23 +1,32 @@
-import clear_icon from "../assets/clear.png";
-import cloud_icon from "../assets/cloud.png";
-import drizzie_icon from "../assets/drizzie.png";
-import rain_icon from "../assets/rain.png";
+import clear_day from "../assets/01d.png";
+import clear_night from "../assets/01n.png";
+import scattered_clouds_day from "../assets/03d.png";
+import scattered_clouds_night from "../assets/03n.png";
+import few_clouds_day from "../assets/few_clouds_day.png";
+import few_clouds_night from "../assets/few_clouds_night.png";
+import broken_clouds_day from "../assets/broken_clouds_day.png";
+import broken_clouds_night from "../assets/broken_clouds_night.png";
+import shower_rain_day from "../assets/shower_rain_day.png";
+import shower_rain_night from "../assets/shower_rain_night.png";
 import snow_icon from "../assets/snow.png";
-import cloudy_icon from "../assets/cloudy.png";
+import mist_day from "../assets/mist_day.png";
+import mist_night from "../assets/mist_night.png";
 
 export const allicons = {
-  "01d": clear_icon,
-  "01n": clear_icon,
-  "02d": cloud_icon,
-  "02n": cloud_icon,
-  "03d": cloud_icon,
-  "03n": cloudy_icon,
-  "04d": cloudy_icon,
-  "04n": drizzie_icon,
-  "09d": rain_icon,
-  "09n": rain_icon,
-  "10d": rain_icon,
-  "10n": rain_icon,
+  "01d": clear_day,
+  "01n": clear_night,
+  "02d": few_clouds_day,
+  "02n": few_clouds_night,
+  "03d": scattered_clouds_day,
+  "03n": scattered_clouds_night,
+  "04d": broken_clouds_day,
+  "04n": broken_clouds_night,
+  "09d": shower_rain_day,
+  "09n": shower_rain_night,
+  "10d": shower_rain_day,
+  "10n": shower_rain_night,
   "13d": snow_icon,
   "13n": snow_icon,
+  "50d": mist_day,
+  "50n": mist_night,
 };
